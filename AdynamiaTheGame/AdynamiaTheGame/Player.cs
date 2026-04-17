f35d7e2d2161238c000dffb3fdd23b8d6aa423f6
Goutham Narayana Pedinedi <292437@friscoisd.org>
2026-04-17 11:54:50 -0500

    Fixed namespaces

--- AdynamiaTheGame/AdynamiaTheGame/Player.cs
+++ AdynamiaTheGame/AdynamiaTheGame/Player.cs
@@ -9,7 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Adynamia
+namespace AdynamiaTheGame
 {
     // For y'all
     // Most of this is lowk self-explanatory code
